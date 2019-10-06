47a8f89e422eed71ea3aac9f2facc0415a3d367c
Hoferj867 <55890926+Hoferj867@users.noreply.github.com>
2019-10-06 15:28:34 +0300

    Add files via upload

---
+++ Lab_1nember3/Lab_1nember3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lab_1
+{
+    class Lab_1nember3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество часов:");
+            int hors = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите количество минут:");
+            int minutes = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите количество секунд:");
+            int seconds = Convert.ToInt32(Console.ReadLine());
+            
+            double degree = (hors * 30) + (minutes * 0.5) + (seconds*(0.5/60.0));
+
+            Console.WriteLine($"градусов: {degree}");
+
+
+        }
+    }
+}
---
+++ Lub_1number4/Lub_1number4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lub_1number4
+{
+    class number4
+    {
+        static int x = Convert.ToInt32(Console.ReadLine());
+        static int y = Convert.ToInt32(Console.ReadLine());
+        static void Main(string[] args)
+        {
+            x += y;
+            y -= x;
+            x += y;
+            y *= -1;
+
+            Console.WriteLine($"x = {x}");
+            Console.WriteLine($"y = {y}");
+        }
+    }
+}
