54136c98789cb2e940c1c49dabf0f9d830c5cae9
Hoferj867 <55890926+Hoferj867@users.noreply.github.com>
2019-10-06 15:19:34 +0300

    Add files via upload

---
+++ Lab_1number8/Lab_1number8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lab_1number8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите любое действительное число x:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int a = (int)Math.Pow(x, 4);
+            int b = (int)Math.Pow(x, 3);
+            int c = (int)Math.Pow(x, 2);
+            int calculation = 3 * a - 5 * b + 2 * c - x + 7;
+            Console.WriteLine($"произведение = {calculation}");
+
+        }
+    }
+}
