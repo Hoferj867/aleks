cbab161f76fe156a66c53917c3d6c994ae170f36
Hoferj867 <55890926+Hoferj867@users.noreply.github.com>
2019-10-06 15:35:42 +0300

    Add files via upload

---
+++ num7/Lab_1number7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lab_1number7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 3-значное число:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num > 99 & num<1000)
+                {
+                int hundredth = num / 100;
+                num = num % 100;
+                int tenth = num / 10;
+                num = num % 10;
+                int reversed = (num * 100) + (tenth * 10) + hundredth;
+                Console.WriteLine($"Перевёрнутые 3-значное чило:{reversed}");
+            }
+            else
+            {
+                Console.WriteLine("Число не трёхзначное!");
+            }
+        }
+    }
+}
---
+++ number5/Lab_1number5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab_1number5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длины двух катетов a и b:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int s = (a * b) / 2;
+            int c = (a * a) + (b * b);
+            int p = a + b + c;
+            Console.WriteLine($"площадь = {s}; периметр = {p}");
+        }
+    }
+}
