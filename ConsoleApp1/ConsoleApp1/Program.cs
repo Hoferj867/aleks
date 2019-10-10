54f7c39e736d260197342d5c20b6c180ba7c42d9
hoferj <55890926+Hoferj867@users.noreply.github.com>
2019-10-10 20:16:12 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите а");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите b");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите t");
+            double t = Convert.ToDouble(Console.ReadLine());
+            double e = 2.718;
+            double y = (Math.Pow(e, -b * t) * Math.Sin(a * t + b) - Math.Sqrt(Math.Abs(b * t + a)));
+            double s = (b * Math.Sin(a * Math.Pow(t, 2) * Math.Cos(2 * t)) - 1);
+            Console.WriteLine($"y={y} s={s}");
+
+        }
+    }
+}
---
+++ Lab_1number10/Lab_1number10/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Lab_1number10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите название 1 телепередачи");
+            string name = Console.ReadLine();
+            Console.WriteLine("Введите ФИО 1 ведущего");
+            string initils = Console.ReadLine();
+            Console.WriteLine("Введите рейтинг 1 телепередачи (От 0 до 5)");
+            int rating = Convert.ToInt32(Console.ReadLine());
+            if (rating>5 | rating<0)
+            {
+                Console.WriteLine("Ошибка");
+            }
+            Console.WriteLine("Введите тип 1 телепередачи (И, А или Т)");
+            string format = Console.ReadLine();
+            Console.WriteLine("Введите название 2 телепередачи");
+            string name1 = Console.ReadLine();
+            Console.WriteLine("Введите ФИО 2 ведущего");
+            string initils1 = Console.ReadLine();
+            Console.WriteLine("Введите рейтинг 2 телепередачи (От 0 до 5)");
+            int rating1 = Convert.ToInt32(Console.ReadLine());
+            if (rating1 > 5 | rating < 0)
+            {
+                Console.WriteLine("Ошибка");
+            }
+            Console.WriteLine("Введите тип 3 телепередачи(И, А или Т)");
+            string format1 = Console.ReadLine();
+            Console.WriteLine("Введите название 3 телепередачи");
+            string name2 = Console.ReadLine();
+            Console.WriteLine("Введите ФИО 3 ведущего");
+            string initils2 = Console.ReadLine();
+            Console.WriteLine("Введите рейтинг 3 телепередачи (От 0 до 5)");
+            int rating2 = Convert.ToInt32(Console.ReadLine());
+            if (rating2 > 5 | rating < 0)
+            {
+                Console.WriteLine("Ошибка");
+            }
+            Console.WriteLine("Введите тип 3 телепередачи (И, А или Т");
+            string format2 = Console.ReadLine();
+            Console.WriteLine("________________________________________________________________________________");
+            Console.WriteLine("|                               Телепередачи                                    |");
+            Console.WriteLine("|_______________________________________________________________________________|");
+            Console.WriteLine("| Телепередача             |  Ведущий            |  Рейтинг        |  Тип       |");
+            Console.WriteLine("|__________________________|_____________________|_________________|____________|");
+           Console.WriteLine($"|{name}                    |  {initils}          | {rating}        |{format}    |");
+            Console.WriteLine("|__________________________|_____________________|_________________|____________|");
+           Console.WriteLine($"|{name1}                   |  {initils1}         | {rating1}       |{format1}   |");
+            Console.WriteLine("|__________________________|_____________________|_________________|____________|");
+           Console.WriteLine($"|{name2}                   |  {initils2}         | {rating2}       |{format2}   |");
+            Console.WriteLine("|__________________________|_____________________|_________________|____________|");
+            Console.WriteLine("|     Пересчисляемый тип: И - игровая;  А - аналитическая; Т – ток-шоу          |");
+            Console.WriteLine("|_______________________________________________________________________________|");
+
+
+
+
+
+
+        }
+    }
+}
